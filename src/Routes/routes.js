e47835558248601dfda4d4cf3406af788443f08c
PedroSoprano <vonhishi@gmail.com>
2022-07-04 15:18:48 -0400

    first commit

---
+++ src/Routes/routes.js
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
+function Ways() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            {
+              /* Componente do caminho */
+            }
+          }
+        />
+        <Route
+          path="about"
+          element={
+            {
+              /* Componente do caminho */
+            }
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default Ways;
