c43488f3711c443a8a9f4d28d8aba52070173c06
Gabriela-LC <gabrielacavalcanti6@gmail.com>
2022-07-12 11:32:46 -0300

    feat: desenvilvido página de produtos reservados pelo consumidor

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -4,23 +4,18 @@ import Home from "../pages/Home";
 import RegisterConsumer from "../components/RegisterConsumer";
 import RegisterSeller from "../components/RegisterSeller";
 import DashboardMercado from "../components/DashboardMercado";
+import ReservedConsumer from "../pages/ReservedConsumer";
 
 function Ways() {
   return (
     <>
       <Routes>
-<<<<<<< HEAD
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/dashboard" element={<DashboardMercado />} />
         <Route exact path="consumer/register" element={<RegisterConsumer />} />
         <Route exact path="seller/register" element={<RegisterSeller />} />
-=======
-        <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="consumer/register" element={<RegisterConsumer />} />
-        <Route path="seller/register" element={<RegisterSeller />} />
->>>>>>> develop
+        <Route exact path="consumer/reserved" element={<ReservedConsumer />} />
       </Routes>
     </>
   );
