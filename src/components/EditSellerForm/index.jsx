576c62f98372472163456e6610bb5a17dd3c2265
lorenammp <lorenammpaula@gmail.com>
2022-07-12 18:44:23 -0300

    feat: desenvolvido formulário para editar dados do vendedor pelo dashboard

--- src/components/EditSellerForm/index.jsx
+++ src/components/EditSellerForm/index.jsx
@@ -4,6 +4,13 @@ import { useEffect, useState } from "react";
 
 import axios from "axios";
 
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 function EditSellerForm() {
   const [sellerData, setSellerData] = useState([]);
   const [sellerAddress, setSellerAddress] = useState([]);
@@ -27,8 +34,51 @@ function EditSellerForm() {
       .catch((err) => console.log(err));
   });
 
+  const notifyError = () =>
+    toast.error("Usuário não editado. Verifique os dados enviados.");
+
+  const notifySuccess = () => toast.success("Usuário editado com sucesso!");
+
+  const editSellerData = (data) => {
+    axios
+      .patch(`${base_URL}/users/${userId}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => notifySuccess())
+      .catch((err) => notifyError());
+  };
+
+  const schema = yup.object().shape({
+    name: yup.string(),
+    email: yup.string(),
+    cnpj: yup.string(),
+    rua: yup.string(),
+    numero: yup.string(),
+    bairro: yup.string(),
+    cidade: yup.string(),
+    estado: yup.string(),
+    avatarUrl: yup.string(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const handleEditSeller = (data) => {
+    for (let key in data) {
+      if (data[key] === "") {
+        delete data[key];
+      }
+    }
+    editSellerData(data);
+  };
+
   return (
-    <form className="editSellerForm">
+    <form className="editSellerForm" onSubmit={handleSubmit(handleEditSeller)}>
       <div className="formTitle">Editar Informações</div>
 
       <div className="formGroup">
@@ -39,7 +89,9 @@ function EditSellerForm() {
           type="text"
           placeholder="Nome"
           defaultValue={sellerData.name}
+          {...register("name")}
         />
+        <div className="formErrors">{errors.name?.message}</div>
       </div>
 
       <div className="formGroup">
@@ -50,7 +102,9 @@ function EditSellerForm() {
           type="email"
           placeholder="E-mail"
           defaultValue={sellerData.email}
+          {...register("email")}
         />
+        <div className="formErrors">{errors.email?.message}</div>
       </div>
 
       <div className="formGroup">
@@ -61,7 +115,9 @@ function EditSellerForm() {
           type="number"
           placeholder="CNPJ"
           defaultValue={sellerData.cnpj}
+          {...register("cnpj")}
         />
+        <div className="formErrors">{errors.cnpj?.message}</div>
       </div>
 
       <div className="smallInput">
@@ -73,7 +129,9 @@ function EditSellerForm() {
             type="text"
             placeholder="Rua"
             defaultValue={sellerAddress.rua}
+            {...register("rua")}
           />
+          <div className="formErrors">{errors.rua?.message}</div>
         </div>
         <div className="formGroup">
           <label className="editFormLabel">Número</label>
@@ -83,7 +141,9 @@ function EditSellerForm() {
             type="number"
             placeholder="Número"
             defaultValue={sellerAddress.numero}
+            {...register("numero")}
           />
+          <div className="formErrors">{errors.numero?.message}</div>
         </div>
       </div>
 
@@ -96,7 +156,9 @@ function EditSellerForm() {
             type="text"
             placeholder="Bairro"
             defaultValue={sellerAddress.bairro}
+            {...register("bairro")}
           />
+          <div className="formErrors">{errors.bairro?.message}</div>
         </div>
         <div className="formGroup">
           <label className="editFormLabel">Cidade</label>
@@ -106,7 +168,9 @@ function EditSellerForm() {
             type="text"
             placeholder="Cidade"
             defaultValue={sellerAddress.cidade}
+            {...register("cidade")}
           />
+          <div className="formErrors">{errors.cidade?.message}</div>
         </div>
       </div>
 
@@ -118,17 +182,21 @@ function EditSellerForm() {
           type="text"
           placeholder="Estado"
           defaultValue={sellerAddress.estado}
+          {...register("estado")}
         />
+        <div className="formErrors">{errors.estado?.message}</div>
       </div>
 
       <div className="formGroup">
-        <label className="editFormLabel">URL do avatar</label>
+        <label className="editFormLabel">Avatar</label>
         <input
           className="editFormInput"
           id="avatar"
           type="text"
           placeholder="URL do avatar"
+          {...register("avatar")}
         />
+        <div className="formErrors">{errors.avatar?.message}</div>
       </div>
 
       <button className="saveEditBtn">Salvar</button>
