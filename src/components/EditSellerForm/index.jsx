c8ab7ae8e2a0619e61f844f84bce93684437c035
lorenammp <lorenammpaula@gmail.com>
2022-07-12 18:13:24 -0300

    feat: desenvolvido formulário para edição de dados do seller

--- src/components/EditSellerForm/index.jsx
+++ src/components/EditSellerForm/index.jsx
@@ -1,130 +1,137 @@
 import "./style.css";
 
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
 function EditSellerForm() {
+  const [sellerData, setSellerData] = useState([]);
+  const [sellerAddress, setSellerAddress] = useState([]);
+
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("id");
+
+  const base_URL = "https://ecomarketapi.herokuapp.com";
+
+  useEffect(() => {
+    axios
+      .get(`${base_URL}/users/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setSellerData(response.data);
+        setSellerAddress(response.data.adress);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <form className="editSellerForm">
       <div className="formTitle">Editar Informações</div>
 
-      {/* <div className="formGroup">
-        <label className="editFormLabel" for="name">
-          Nome
-        </label>
+      <div className="formGroup">
+        <label className="editFormLabel">Nome</label>
         <input
           className="editFormInput"
           id="name"
           type="text"
           placeholder="Nome"
-          value="Mercado Rápido"
+          defaultValue={sellerData.name}
         />
       </div>
 
       <div className="formGroup">
-        <label className="editFormLabel" for="email">
-          E-mail
-        </label>
+        <label className="editFormLabel">E-mail</label>
         <input
           className="editFormInput"
           id="email"
           type="email"
           placeholder="E-mail"
-          value="mrapido@mail.com"
+          defaultValue={sellerData.email}
         />
       </div>
 
       <div className="formGroup">
-        <label className="editFormLabel" for="cnpj">
-          CNPJ
-        </label>
+        <label className="editFormLabel">CNPJ</label>
         <input
           className="editFormInput"
           id="cnpj"
           type="number"
           placeholder="CNPJ"
-          value="01234567890"
+          defaultValue={sellerData.cnpj}
         />
       </div>
 
       <div className="smallInput">
         <div className="formGroup">
-          <label className="editFormLabel" for="rua">
-            Rua
-          </label>
+          <label className="editFormLabel">Rua</label>
           <input
             className="editFormInputSmall"
             id="rua"
             type="text"
             placeholder="Rua"
-            value="Rua Amaranto"
+            defaultValue={sellerAddress.rua}
           />
         </div>
         <div className="formGroup">
-          <label className="editFormLabel" for="numero">
-            Número
-          </label>
+          <label className="editFormLabel">Número</label>
           <input
             className="editFormInputSmall"
             id="numero"
             type="number"
             placeholder="Número"
-            value="2986"
+            defaultValue={sellerAddress.numero}
           />
         </div>
       </div>
 
       <div className="smallInput">
         <div className="formGroup">
-          <label className="editFormLabel" for="bairro">
-            Bairro
-          </label>
+          <label className="editFormLabel">Bairro</label>
           <input
             className="editFormInputSmall"
             id="bairro"
             type="text"
             placeholder="Bairro"
-            value="Santo Antônio"
+            defaultValue={sellerAddress.bairro}
           />
         </div>
         <div className="formGroup">
-          <label className="editFormLabel" for="cidade">
-            Cidade
-          </label>
+          <label className="editFormLabel">Cidade</label>
           <input
             className="editFormInputSmall"
             id="cidade"
             type="text"
             placeholder="Cidade"
-            value="Belo Horizonte"
+            defaultValue={sellerAddress.cidade}
           />
         </div>
       </div>
 
       <div className="formGroup">
-        <label className="editFormLabel" for="estado">
-          Estado
-        </label>
+        <label className="editFormLabel">Estado</label>
         <input
           className="editFormInput"
           id="estado"
           type="text"
           placeholder="Estado"
-          value="Minas Gerais"
+          defaultValue={sellerAddress.estado}
         />
       </div>
 
       <div className="formGroup">
-        <label className="editFormLabel" for="avatar">
-          URL do avatar
-        </label>
+        <label className="editFormLabel">URL do avatar</label>
         <input
           className="editFormInput"
           id="avatar"
           type="text"
           placeholder="URL do avatar"
-          value="mrapido@mail.com"
         />
       </div>
 
-      <button className="saveEditBtn">Salvar</button> */}
+      <button className="saveEditBtn">Salvar</button>
     </form>
   );
 }
